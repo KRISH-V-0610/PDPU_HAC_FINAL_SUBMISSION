1903f799abd6c36cdfafba09728ba46487b0718f
KRISH0830 <166995651+KRISH0830@users.noreply.github.com>
2025-03-23 07:40:32 +0530

    finalll

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  origin: ["https://pdpu-hac-final-submission-4.onrender.com/"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
   credentials: true
