10cf5fc9af0b6292996cb5b9b3c36bbf9cc33e7d
KRISH0830 <166995651+KRISH0830@users.noreply.github.com>
2025-03-23 08:41:36 +0530

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const cors = require("cors");
 app.use(cors({ origin: "*", credentials: true }));
 
 // Increase payload size limit for file uploads
